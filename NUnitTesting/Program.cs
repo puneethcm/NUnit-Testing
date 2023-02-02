b0ba7d08af530492713257a97c6e0739b90f86f7
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-02 08:53:54 +0530

    Temperature Conversion

--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -5,12 +5,15 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("\nWelcome to NUnit Testing Program");
-        Console.WriteLine("\n1:Vending Machine");
+        Console.WriteLine("\n1:Vending Machine\n2:Temperatute Conversion");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
         {
             case 1:
                 Vending.GetVending();
+                break;
+            case 2:
+                TempConversion.FindTempConversion();
                 break;
         }
         Console.ReadLine();
