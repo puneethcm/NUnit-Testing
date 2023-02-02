2cce3b01b5ad9b7631221d248db8b409b5f19655
puneethcm <120664786+puneethcm@users.noreply.github.com>
2023-02-02 16:10:50 +0530

    Update Vending.cs

--- NUnitTesting/Vending.cs
+++ NUnitTesting/Vending.cs
@@ -1,29 +1,30 @@
 using System;
 namespace NUnitTesting
 {
-	public class Vending
-	{
-		public static void GetVending()
-		{
-            		Console.WriteLine("Enter the Amount");
-            		int amount = Convert.ToInt32(Console.ReadLine());
-			int[] notes = new int[] { 2000,1000 , 500, 200, 100, 50, 20, 10, 5, 2, 1 };
-			int[] noteCounter = new int[11];
-
-            		for (int i = 0; i < 11; i++)
-            		{
-                		noteCounter[i] = amount / notes[i];
-                		amount = amount % notes[i];
-            		}
-            		Console.WriteLine("Currency amount ->");
-            		for(int i = 0; i < 11; i++)
-            		{
-                		if (noteCounter[i] != 0)
-                		{
-                    		Console.WriteLine(notes[i] + " : " + noteCounter[i]);
-                		}
-            		}
-        	}
-	}
+    public class Vending
+    {
+        public static void GetVending()
+        {
+            Console.WriteLine("Enter the Amount");
+            int amount = Convert.ToInt32(Console.ReadLine());
+            int[] notes = new int[] { 2000, 1000, 500, 200, 100, 50, 20, 10, 5, 2, 1 };
+            int[] noteCounter = new int[11];
+            for (int i = 0; i < 11; i++)
+            {
+                if (amount >= notes[i])
+                {
+                    noteCounter[i] = amount / notes[i];
+                    amount = amount % notes[i];
+                }
+            }
+            Console.WriteLine("Currency amount ->");
+            for (int i = 0; i < 11; i++)
+            {
+                if (noteCounter[i] != 0)
+                {
+                    Console.WriteLine(notes[i] + " : " + noteCounter[i]);
+                }
+            }
+        }
+    }
 }
-
