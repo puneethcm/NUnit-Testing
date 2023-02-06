49c7159f1add9f0d9a0fa474e2d2712e993f574e
puneethcm <puneethkumarcm8050@gmail.com>
2023-02-06 08:01:38 +0530

    Monthly Payment

---
+++ NUnitTesting/MonthlyPayment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace NUnitTesting
+{
+	public class MonthlyPayment
+	{
+        public static double P, Y, R, monthlyPayment, n, r;
+        public static void FindMonthlyPayment()
+        {
+            Console.WriteLine("Enter th loan amount");
+            double R = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the interest");
+            double P = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the number of years");
+            int Y = Convert.ToInt32(Console.ReadLine());
+
+            n = 12 * Y;
+            r = R / (12 * 100);
+            monthlyPayment = (P * r) / (1 - (Math.Pow((1 + r), -n)));
+            Console.WriteLine("Monthly Payment is:" + monthlyPayment);
+        }
+    }
+}
+
--- NUnitTesting/Program.cs
+++ NUnitTesting/Program.cs
@@ -5,7 +5,7 @@ internal class Program
     private static void Main(string[] args)
     {
         Console.WriteLine("\nWelcome to NUnit Testing Program");
-        Console.WriteLine("\n1:Vending Machine\n2:Temperatute Conversion\n3:Day Of Week\n4:Square Root\n5:Decimel to Binary");
+        Console.WriteLine("\n1:Vending Machine\n2:Temperatute Conversion\n3:Day Of Week\n4:Square Root\n5:Decimel to Binary\n6:MonthlyPayment");
         int option = Convert.ToInt32(Console.ReadLine());
         switch (option)
         {
@@ -23,6 +23,9 @@ internal class Program
                 break;
             case 5:
                 DecimelToBinary.FindDecimelToBinary();
+                break;
+            case 6:
+                MonthlyPayment.FindMonthlyPayment();
                 break;
         }
         Console.ReadLine();
